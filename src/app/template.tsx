4fc87872fea3582dff158283713c5496f405823d
xuefei <junmo@yscredit.com>
2025-03-17 23:59:39 +0800

    nextjs test

---
+++ src/app/template.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react'
+import { usePathname } from 'next/navigation';
+
+const linkData = [
+  {name:'首页',path:'/dashboard'}
+]
+const accessLink = ['/','/dashboard']
+
+  export default function template({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    const pathname = usePathname();
+   if(!accessLink.includes(pathname)) return (
+    <div>
+      {children}
+    </div>
+   );
+
+    return (
+      <div className='bg-blue-500'>
+        <h1 className=''>这是模版</h1>
+        {children}
+         
+      </div>
+    );
+  }
